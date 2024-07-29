0c58ba7c84efe6efdac343414b76966df3262f08
JhonSebasZ <zunigalopezjhonsebastian@gmail.com>
2024-07-28 19:46:02 -0500

    first commit

---
+++ src/main/java/com/prueba_proteccion/PruebaProteccionApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_proteccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaProteccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaProteccionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba_proteccion/PruebaProteccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_proteccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaProteccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
